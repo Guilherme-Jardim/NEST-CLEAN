8c72215e0484a6a20f5d84d7de4fbfc12e915949
guilherme-jardim <guilhermejardimj@gmail.com>
2024-06-27 14:10:41 -0300

    implement config module

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,8 +1,5 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
-import * as dotenv from 'dotenv'
-
-dotenv.config()
 
 @Injectable()
 export class PrismaService
