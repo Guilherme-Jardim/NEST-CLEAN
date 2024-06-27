24757bd258bcc972335f7e7410152cacb7b3cb4a
guilherme-jardim <guilhermejardimj@gmail.com>
2024-06-27 10:22:51 -0300

    add create-account.controller

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { PrismaClient } from '@prisma/client'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  public client: PrismaClient
-
   constructor() {
     super({
       log: ['warn', 'error'],
